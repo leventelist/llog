ca1e7f6d72a4d048c8445462d752961a50790a5d
Levente <leventelist@gmail.com>
2013-07-19 23:06:39 +0200

    TX number
    TX number read from log file
    -n command line option can determine TX number

--- llog/llog.h
+++ llog/llog.h
@@ -14,7 +14,6 @@
 #define LIST_LEN 50
 #define LINE_LEN 1024
 #define SUBSTR_LEN 512
-#define CSV_LIST_LEN 3
 
 #define OK 0
 #define FILE_ERR 1
@@ -23,10 +22,26 @@
 #define CANCEL_SEQ ":c"
 #define PROMPT ": "
 
+/*CSV stuff*/
+#define CSV_LIST_LEN 17
 /*CSV field positions*/
 #define CSV_DATE_POS 0
 #define CSV_TIME_POS 1
 #define CSV_CALL_POS 2
+#define CSV_TXRST_POS 3
+#define CSV_RXRST_POS 4
+#define CSV_QTH_POS 5
+#define CSV_QRA_POS 6
+#define CSV_NAME_POS 7
+#define CSV_QRG_POS 8
+#define CSV_MODE_POS 9
+#define CSV_RXNR_POS 10
+#define CSV_TXNR_POS 11
+#define CSV_COMMENT_POS 12
+#define CSV_LOCAL_QTH_POS 13
+#define CSV_LOCAL_QRA_POS 14
+#define CSV_LOCAL_RIG_POS 15
+#define CSV_LOCAL_ANT_POS 16
 
 typedef struct {
 
