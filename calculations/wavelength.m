94f9d904d32809c27177d206f27d507d8ef24df1
Levente Kovacs <leventelist@gmail.com>
2019-03-11 19:19:25 +0100

    Add wavelength calculations.
    Signed-off-by: Levente Kovacs <leventelist@gmail.com>

---
+++ calculations/wavelength.m
@@ -0,0 +1,17 @@
+#Speed of light in space and air
+c = 299792458
+
+#velocity factor of the cable
+vf = 0.97
+
+#frequency
+f = 14.050E6
+
+f_MHz = f / 1E6
+
+lambda = vf * c / f
+
+half_lambda = lambda / 2
+
+quarter_lambda = lambda / 4
+
