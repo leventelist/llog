ca075a3eb5d7b12717b57b00dc73759cca5a54cc
Levente Kovacs <leventelist@gmail.com>
2020-04-13 02:32:18 +0200

    Header update

--- main.c
+++ main.c
@@ -1,5 +1,5 @@
 /*	This is llog, a minimalist HAM logging software.
- *	Copyright (C) 2013-2019  Levente Kovacs
+ *	Copyright (C) 2013-2020  Levente Kovacs
  *
  *	This program is free software; you can redistribute it and/or modify
  *	it under the terms of the GNU General Public License as published by
