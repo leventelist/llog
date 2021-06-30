bb7e55a956a18b0ffcc9174f48ff4b8e2934c792
Levente Kovacs <leventelist@gmail.com>
2021-06-30 18:06:36 +0200

    Add python library for llog databases

---
+++ libllog/test.py
@@ -0,0 +1,23 @@
+#!/bin/python3
+
+import llog
+
+def main():
+    llogd = llog.Llog("log.sqlite")
+    llogd.open_database()
+    qsos = llogd.get_qsos("call='R2AIJ'")
+    for qso in qsos:
+        print(qso['rowid'], qso['date'] + " " + qso['UTC'] + " " + qso['call'])
+    qsos = llogd.get_qsos()
+    for qso in qsos:
+        print(qso['rowid'], qso['date'] + " " + qso['UTC'] + " " + qso['call'])
+
+    stations = llogd.get_stations()
+    for station in stations:
+        print(str(station['rowid']) + " " + station['name'] + " " + station["CALL"])
+    stations = llogd.get_stations(1)
+    for station in stations:
+        print(str(station['rowid']) + " " + station['name'] + " " + station["CALL"])
+
+if __name__ == '__main__':
+    main()
